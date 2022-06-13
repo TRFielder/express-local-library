f123b460648297af3256a5c8b6bff330d1046e28
Tom Fielder <tom.fielder97@gmail.com>
2022-06-13 23:57:04 +0100

    Add bookInstance view

--- populatedb.js
+++ populatedb.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 
+// mongodb+srv://cooluser:coolpassword@cluster0.sjtvc.mongodb.net/?retryWrites=true&w=majority
+
 console.log(
   "This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true"
 );
